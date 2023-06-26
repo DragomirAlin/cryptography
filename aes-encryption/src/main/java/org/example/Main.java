8dc9a9275f730ae87ef22af57e4589050482e6d2
Dragomir Alin <dragomirdanielalin@gmail.com>
2023-06-26 20:56:00 +0300

    initial commit

---
+++ aes-encryption/src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        //Creating a Signature object
+        Signature sign = Signature.getInstance("SHA256withRSA");
+
+        //Creating KeyPair generator object
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+
+        //Initializing the key pair generator
+        keyPairGen.initialize(2048);
+
+        //Generate the pair of keys
+        KeyPair pair = keyPairGen.generateKeyPair();
+
+        //Getting the public key from the key pair
+        PublicKey publicKey = pair.getPublic();
+
+        //Creating a Cipher object
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        //Initializing a Cipher object
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        //Add data to the cipher
+        byte[] input = "Hello world".getBytes();
+        cipher.update(input);
+
+        //encrypting the data
+        byte[] cipherText = cipher.doFinal();
+        System.out.println( new String(cipherText, "UTF8"));
+
+        //Initializing the same cipher for decryption
+        cipher.init(Cipher.DECRYPT_MODE, pair.getPrivate());
+
+        //Decrypting the text
+        byte[] decipheredText = cipher.doFinal(cipherText);
+        System.out.println(new String(decipheredText));
+    }
+}
