5a901400722496ae460078662781ac0efbdcb908
Dragomir Alin <dragomirdanielalin@gmail.com>
2023-06-28 20:07:54 +0300

    add rsa encryption

--- asymmetric-encryption/src/main/java/org/example/Main.java
+++ asymmetric-encryption/src/main/java/org/example/Main.java
@@ -1,7 +1,78 @@
 package org.example;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+/**
+ * RSA, or in other words Rivest–Shamir–Adleman, is an asymmetric cryptographic algorithm. It differs from symmetric algorithms like DES or AES by having two keys.
+ * A public key that we can share with anyone is used to encrypt data. And a private one that we keep only for ourselves and it's used for decrypting the data
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        rsaEncryption();
+    }
+
+    private static void rsaEncryption() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        KeyPair keyPair = generateRSAKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = managePublicKey(keyPair);
+
+        String message = "Hello World!";
+        String encryptedMessage = encrypt(message, publicKey);
+        System.out.println("Encrypted message: " + encryptedMessage);
+        String decryptedMessage = decrypt(encryptedMessage, privateKey);
+        System.out.println("Decrypted message: " + decryptedMessage);
+    }
+
+    private static String encrypt(String message, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedMessageBytes = cipher.doFinal(secretMessageBytes);
+        return Base64.getEncoder().encodeToString(encryptedMessageBytes);
+    }
+
+    private static String decrypt(String encryptedMessage, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] encryptedMessageBytes = Base64.getDecoder().decode(encryptedMessage);
+        byte[] decryptedMessageBytes = cipher.doFinal(encryptedMessageBytes);
+        return new String(decryptedMessageBytes);
+    }
+
+    private static PublicKey managePublicKey(KeyPair keyPair) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        PublicKey publicKey = keyPair.getPublic();
+
+        try (FileOutputStream fos = new FileOutputStream("public.key")) {
+            fos.write(publicKey.getEncoded());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        File publicKeyFile = new File("public.key");
+        byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+
+        return keyFactory.generatePublic(publicKeySpec);
+    }
+
+    public static KeyPair generateRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        return generator.generateKeyPair();
     }
 }
